c616afa59c8404d79b38397bb712ccd31ccc531f
Fabian <orleyfabian.r@gmail.com>
2025-10-15 00:29:07 -0500

    Add implementation for conceptual deletion with displacement in an array

---
+++ ejercicio_6.py
@@ -0,0 +1,6 @@
+# 6) Borrado conceptual con desplazamiento (Intermedio)
+# – Dado arr = [4, 6, 8, 10, 12], borra el elemento en índice 2 
+# desplazando a la izquierda los siguientes.
+# – Rellena el último espacio con None y muestra el arreglo resultante
+arr = [4, 6, 8, 10, 12]
+
