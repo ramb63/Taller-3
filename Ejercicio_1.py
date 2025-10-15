d8cc95987617ad0a6a9177e3fc292fad94cdfdec
Fabian <orleyfabian.r@gmail.com>
2025-10-14 20:51:44 -0500

    Add initial implementation for list indexing exercises

---
+++ Ejercicio_1.py
@@ -0,0 +1,13 @@
+#1) Índices y acceso (Básico)
+# Dado a = [7, 3, 9, 5, 9], escribe qué imprimen:
+# 1) print(a[0])
+# 2) print(a[3])
+# 3) print(a[len(a)-1])
+# 4) Explica brevemente por qué el último índice es len(a)-1.
+
+a = [7, 3, 9, 5, 9]
+print(a[0])  
+print(a[3])
+print(a[len(a)-1])      
+# Nos muestra el último elemento de la lista ya que el -1,
+# busca el ultimo del conjunto de a.
