5265f43b3e6f4c806f08ca66d42e435c2c7a91ba
Fabian <orleyfabian.r@gmail.com>
2025-10-14 23:24:23 -0500

    Add initial implementation for printing indexed age values

---
+++ Ejercicio_3.py
@@ -0,0 +1,9 @@
+#Llenado con datos simulados (Básico)
+#Crea un arreglo de 5 edades inicializado en cero y 'cárgalo' con 
+# valores = [21, 18, 30, 25, 19].
+#Imprime en cada línea: índice → valor.
+valores = [21, 18, 30, 25, 19]
+indice = 0 
+for valor in valores:
+    print(indice, '→', valor)
+    indice += 1
