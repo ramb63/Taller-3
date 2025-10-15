f89d3e6118934b557626b6a6003fb8ae94d6a405
Fabian <orleyfabian.r@gmail.com>
2025-10-15 18:30:44 -0500

    Refactor bubble sort trace output to print after each pass

--- Ejercicio_8.py
+++ Ejercicio_8.py
@@ -3,7 +3,6 @@
 # – Escribe la traza tras cada pasada (no es necesario listar cada comparación) y luego imprime el
 # arreglo ordenado
 a = [5, 1, 4, 2]
-print(a)
 for i in range(1, len(a)):
     actual = a[i]
     j = i - 1
@@ -11,5 +10,4 @@ for i in range(1, len(a)):
         a[j + 1] = a[j]
         j -= 1
     a[j + 1] = actual
-
-print(a)
+    print(a)
