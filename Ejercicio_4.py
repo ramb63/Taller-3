0781a5497114b5c37f94aead427d7320b00b4553
Fabian <orleyfabian.r@gmail.com>
2025-10-14 23:39:40 -0500

    Add basic implementation for updating an element in a list by index

---
+++ Ejercicio_4.py
@@ -0,0 +1,6 @@
+# 4) Actualización por índice (Básico)
+# – Con nombres = ['Ana','Luis','Mar','Sofi'], cambia 'Mar' por 'María'.
+# – Muestra el elemento actualizado y luego el arreglo completo
+nombres = ['Ana','Luis','Mar','Sofi']
+nombres[2] = 'María'
+print("el elemento actualizado", nombres)
